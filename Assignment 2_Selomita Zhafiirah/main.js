d846e79dc93170a1c47646074a06c4b1adaf8757
seraaaz <selomita.zhafiirah@gmail.com>
2022-12-06 14:34:01 +0700

    still error

--- Assignment 2_Selomita Zhafiirah/main.js
+++ Assignment 2_Selomita Zhafiirah/main.js
@@ -1,6 +1,9 @@
 var canvas = document.getElementById("myCanvas");
 var gl = canvas.getContext("webgl");
 
+
+
+
 var verticesA1 = [
   //depan
   -0.750000, 0.900000, 0,         0.974, 0.99, 0.00,
@@ -405,6 +408,209 @@ var indices0 = [
   17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
 ];
 
+
+var verticesCube = [
+  // Face A       // Red      // Surface orientation
+  -1, -1, -1,     1, 0, 0,    0, 0, -1,   // Index:  0    
+   1, -1, -1,     1, 0, 0,    0, 0, -1,   // Index:  1
+   1,  1, -1,     1, 0, 0,    0, 0, -1,   // Index:  2
+  -1,  1, -1,     1, 0, 0,    0, 0, -1,   // Index:  3
+  // Face B       // Yellow
+  -1, -1,  1,     1, 1, 0,    0, 0, 1,    // Index:  4
+   1, -1,  1,     1, 1, 0,    0, 0, 1,    // Index:  5
+   1,  1,  1,     1, 1, 0,    0, 0, 1,    // Index:  6
+  -1,  1,  1,     1, 1, 0,    0, 0, 1,    // Index:  7
+  // Face C       // Green
+  -1, -1, -1,     0, 1, 0,    -1, 0, 0,   // Index:  8
+  -1,  1, -1,     0, 1, 0,    -1, 0, 0,   // Index:  9
+  -1,  1,  1,     0, 1, 0,    -1, 0, 0,   // Index: 10
+  -1, -1,  1,     0, 1, 0,    -1, 0, 0,   // Index: 11
+  // Face D       // Blue
+   1, -1, -1,     0, 0, 1,    1, 0, 0,    // Index: 12
+   1,  1, -1,     0, 0, 1,    1, 0, 0,    // Index: 13
+   1,  1,  1,     0, 0, 1,    1, 0, 0,    // Index: 14
+   1, -1,  1,     0, 0, 1,    1, 0, 0,    // Index: 15
+  // Face E       // Orange
+  -1, -1, -1,     1, 0.5, 0,  0, -1, 0,   // Index: 16
+  -1, -1,  1,     1, 0.5, 0,  0, -1, 0,   // Index: 17
+   1, -1,  1,     1, 0.5, 0,  0, -1, 0,   // Index: 18
+   1, -1, -1,     1, 0.5, 0,  0, -1, 0,   // Index: 19
+  // Face F       // White
+  -1,  1, -1,     1, 1, 1,    0, 1, 0,    // Index: 20
+  -1,  1,  1,     1, 1, 1,    0, 1, 0,    // Index: 21
+   1,  1,  1,     1, 1, 1,    0, 1, 0,    // Index: 22
+   1,  1, -1,     
+];
+
+var indicesCube = [
+    0, 1, 2,     0, 2, 3,     // Face A
+    4, 5, 6,     4, 6, 7,     // Face B
+    8, 9, 10,    8, 10, 11,   // Face C
+    12, 13, 14,  12, 14, 15,  // Face D
+    16, 17, 18,  16, 18, 19,  // Face E
+    20, 21, 22,  20, 22, 23   // Face F     
+];
+
+
+// Create and store data into vertex buffer
+var vertex_buffer = gl.createBuffer ();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+// Create and store data into color buffer
+var color_buffer = gl.createBuffer ();
+gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+// Create and store data into index buffer
+var index_buffer = gl.createBuffer ();
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+/*=================== Shaders =========================*/
+
+var vertCode = 'attribute vec3 position;'+
+   'uniform mat4 Pmatrix;'+
+   'uniform mat4 Vmatrix;'+
+   'uniform mat4 Mmatrix;'+
+   'attribute vec3 color;'+//the color of the point
+   'varying vec3 vColor;'+
+
+   'void main(void) { '+//pre-built function
+      'gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);'+
+      'vColor = color;'+
+   '}';
+
+var fragCode = 'precision mediump float;'+
+   'varying vec3 vColor;'+
+   'void main(void) {'+
+      'gl_FragColor = vec4(vColor, 1.);'+
+   '}';
+
+var vertShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertShader, vertCode);
+gl.compileShader(vertShader);
+
+var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragShader, fragCode);
+gl.compileShader(fragShader);
+
+var shaderProgram = gl.createProgram();
+gl.attachShader(shaderProgram, vertShader);
+gl.attachShader(shaderProgram, fragShader);
+gl.linkProgram(shaderProgram);
+
+/* ====== Associating attributes to vertex shader =====*/
+var Pmatrix = gl.getUniformLocation(shaderProgram, "Pmatrix");
+var Vmatrix = gl.getUniformLocation(shaderProgram, "Vmatrix");
+var Mmatrix = gl.getUniformLocation(shaderProgram, "Mmatrix");
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+var position = gl.getAttribLocation(shaderProgram, "position");
+gl.vertexAttribPointer(position, 3, gl.FLOAT, false,0,0) ;
+
+// Position
+gl.enableVertexAttribArray(position);
+gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+var color = gl.getAttribLocation(shaderProgram, "color");
+gl.vertexAttribPointer(color, 3, gl.FLOAT, false,0,0) ;
+
+// Color
+gl.enableVertexAttribArray(color);
+gl.useProgram(shaderProgram);
+
+/*==================== MATRIX =====================*/
+
+function get_projection(angle, a, zMin, zMax) {
+   var ang = Math.tan((angle*.5)*Math.PI/180);//angle*.5
+   return [
+      0.5/ang, 0 , 0, 0,
+      0, 0.5*a/ang, 0, 0,
+      0, 0, -(zMax+zMin)/(zMax-zMin), -1,
+      0, 0, (-2*zMax*zMin)/(zMax-zMin), 0 
+   ];
+}
+
+var proj_matrix = get_projection(40, canvas.width/canvas.height, 1, 100);
+
+var mov_matrix = [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1];
+var view_matrix = [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1];
+
+// translating z
+view_matrix[14] = view_matrix[14]-6;//zoom
+
+/*==================== Rotation ====================*/
+
+function rotateZ(m, angle) {
+   var c = Math.cos(angle);
+   var s = Math.sin(angle);
+   var mv0 = m[0], mv4 = m[4], mv8 = m[8];
+
+   m[0] = c*m[0]-s*m[1];
+   m[4] = c*m[4]-s*m[5];
+   m[8] = c*m[8]-s*m[9];
+
+   m[1]=c*m[1]+s*mv0;
+   m[5]=c*m[5]+s*mv4;
+   m[9]=c*m[9]+s*mv8;
+}
+
+function rotateX(m, angle) {
+   var c = Math.cos(angle);
+   var s = Math.sin(angle);
+   var mv1 = m[1], mv5 = m[5], mv9 = m[9];
+
+   m[1] = m[1]*c-m[2]*s;
+   m[5] = m[5]*c-m[6]*s;
+   m[9] = m[9]*c-m[10]*s;
+
+   m[2] = m[2]*c+mv1*s;
+   m[6] = m[6]*c+mv5*s;
+   m[10] = m[10]*c+mv9*s;
+}
+
+function rotateY(m, angle) {
+   var c = Math.cos(angle);
+   var s = Math.sin(angle);
+   var mv0 = m[0], mv4 = m[4], mv8 = m[8];
+
+   m[0] = c*m[0]+s*m[2];
+   m[4] = c*m[4]+s*m[6];
+   m[8] = c*m[8]+s*m[10];
+
+   m[2] = c*m[2]-s*mv0;
+   m[6] = c*m[6]-s*mv4;
+   m[10] = c*m[10]-s*mv8;
+}
+
+/*================= Drawing ===========================*/
+var time_old = 0;
+
+var animate = function(time) {
+
+   var dt = time-time_old;
+   rotateZ(mov_matrix, dt*0.005);//time
+   rotateY(mov_matrix, dt*0.002);
+   rotateX(mov_matrix, dt*0.003);
+   time_old = time;
+
+   gl.enable(gl.DEPTH_TEST);
+   gl.depthFunc(gl.LEQUAL);
+   gl.clearColor(0.5, 0.5, 0.5, 0.9);
+   gl.clearDepth(1.0);
+
+   gl.viewport(0.0, 0.0, canvas.width, canvas.height);
+   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+   gl.uniformMatrix4fv(Pmatrix, false, proj_matrix);
+   gl.uniformMatrix4fv(Vmatrix, false, view_matrix);
+   gl.uniformMatrix4fv(Mmatrix, false, mov_matrix);
+   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
+   gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
+   window.requestAnimationFrame(animate);
+}
+animate(0);
+
 const objects = [
   {
     vertices: verticesA1,
@@ -471,8 +677,12 @@ const objects = [
     indices: indices0,
     length: indices0.length,
     type: gl.LINE_LOOP,
-  }
-]
+  },
+  {vertices: verticesCube,
+  indices: indicesCube,
+  length: indicesCube.length,
+  type: gl.TRIANGLES},
+];
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
@@ -655,7 +865,29 @@ const animate2 = () =>{
     draw(objects[6].vertices, objects[6].indices, 0, objects[6].length, objects[6].type);
     draw(objects[7].vertices, objects[7].indices, 0, objects[7].length, objects[7].type);
   }
+
+  const animateCube  = () =>{
+    var model = glMatrix.mat4.create();
+
+    glMatrix.mat4.rotateY(
+      model, model, rotateY
+    );
+
+    var mWorld = gl.getUniformLocation(program, "mWorld");
+    var mView = gl.getUniformLocation(program, "mView");
+    var mProj = gl.getUniformLocation(program, "mProj"); 
+
+    gl.uniformMatrix4fv(mWorld,false, model);
+    gl.uniformMatrix4fv(mView, false, view);
+    gl.uniformMatrix4fv(mProj, false, perspective);
+
+    draw(objects[0].vertices, objects[0].indices, 0, objects[0].length, objects[0].type);
+    draw(objects[1].vertices, objects[1].indices, 0, objects[1].length, objects[1].type);
+    draw(objects[2].vertices, objects[2].indices, 0, objects[2].length, objects[3].type);
+    draw(objects[3].vertices, objects[3].indices, 0, objects[3].length, objects[3].type);
+  }
   
+
   function onKeydown(event) {
     if (event.keyCode == 65 || event.keyCode == 37) { // a / arrow kiri
       rotateY -= 0.2;
@@ -679,6 +911,7 @@ const animate2 = () =>{
     animateH();
     animate2();
     animate0();
+    animateCube();
     requestAnimationFrame(render);
   }
   requestAnimationFrame(render);
